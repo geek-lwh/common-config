7ee18ad77b8c13204aee538f921d9ee2414de2b2
luweihong <luweihong@ahaschool.com>
2020-11-30 15:54:00 +0800

    update 3.7.23

--- src/main/java/com/aha/tech/exception/GlobalException.java
+++ src/main/java/com/aha/tech/exception/GlobalException.java
@@ -5,8 +5,8 @@ package com.aha.tech.exception;
  * 说明:异常类父类,用于抛出异常动态转型, 此abstract异常类不可构造使用
  *
  */
-public abstract class GlobalException extends RuntimeException {
-    private static final long serialVersionUID = -2423279393324395996L;
+public class GlobalException extends RuntimeException {
+
     protected int code;
 
     public GlobalException(String msg, int code) {
